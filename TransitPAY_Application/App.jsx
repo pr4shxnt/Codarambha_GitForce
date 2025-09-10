b5d8252ba13fa1d269d031a5e4b315d22babf62b
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-09-11 00:28:41 +0545

    update

---
+++ TransitPAY_Application/App.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NewAppScreen } from '@react-native/new-app-screen';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
