1e77eb5a34e96550393aedaf272ef1793665660b
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-09-17 17:58:18 +0545

    add favicon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -11,15 +11,22 @@ import { Footer } from "@/components/footer";
 export const metadata: Metadata = {
   title: "TransitPay - Your Digital Travel Wallet",
   description:
-    "Seamless, secure, and smart NFC payments for public transportation. Transform your commute with TransitPay.",
-  generator: "v0.app",
+    "Seamless, secure, and smart NFC payments for public transportation. Transform your commute with TransitPay inside Nepal.",
+  authors: [{ name: "GitForce" }],
+  icons: {
+    icon: [
+      { url: "/icons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/icons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/icons/favicon.ico", type: "image/x-icon" },
+    ],
+    apple: "/icons/apple-touch-icon.png",
+    shortcut: "/icons/favicon.ico",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
