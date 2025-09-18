376071cf8da79657b1956854a68a2b6d8be2e3b2
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-09-18 14:35:02 +0545

    hardware: main.ino updated; only type 4 nfc cards are supported

--- Hardwares/main/main.ino
+++ Hardwares/main/main.ino
@@ -1,75 +1,110 @@
 #include <SPI.h>
 #include <Adafruit_PN532.h>
 
-// SPI pins for NodeMCU/ESP8266
-#define PN532_SCK 14  // D5
-#define PN532_MISO 12 // D6
-#define PN532_MOSI 13 // D7
-#define PN532_SS 2    // D4
+#define PN532_SS   D4
+#define PN532_SCK  D5
+#define PN532_MOSI D7
+#define PN532_MISO D6
 
 Adafruit_PN532 nfc(PN532_SS);
 
-#define TIMEOUT_TAG_WAIT 10000 // 10 seconds
-
 void setup() {
   Serial.begin(115200);
-  while (!Serial) delay(10);
-
-  Serial.println("PN532 HCE Debug Starting...");
+  Serial.println("Initializing PN532...");
 
   nfc.begin();
 
   uint32_t versiondata = nfc.getFirmwareVersion();
   if (!versiondata) {
-    Serial.println("Didn't find PN53x board!");
+    Serial.println("PN532 not found!");
     while (1);
   }
 
-  Serial.print("Found PN5"); Serial.println((versiondata >> 24) & 0xFF, HEX);
-  Serial.print("Firmware ver. "); Serial.print((versiondata >> 16) & 0xFF, DEC);
-  Serial.print('.'); Serial.println((versiondata >> 8) & 0xFF, DEC);
-
-  nfc.SAMConfig(); // configure as normal reader
-  nfc.setPassiveActivationRetries(0xFF);
-
-  Serial.println("Waiting for HCE device...");
+  nfc.SAMConfig(); // configure board
+  Serial.println("Waiting for ISO-DEP / Type 4 card...");
 }
 
 void loop() {
-  uint8_t uid[8];
+  uint8_t success;
+  uint8_t uid[7];
   uint8_t uidLength;
 
-  // Poll for ISO14443A/HCE device
-  bool success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, TIMEOUT_TAG_WAIT);
+  // Detect any NFC card
+  success = nfc.inListPassiveTarget();
 
   if (success) {
-    Serial.println("\nHCE device detected!");
-    Serial.print("UID Length: "); Serial.println(uidLength, DEC);
-    Serial.print("UID: ");
-    for (uint8_t i = 0; i < uidLength; i++) {
-      Serial.print(uid[i], HEX); Serial.print(" ");
-    }
-    Serial.println();
+    Serial.println("Card detected, attempting ISO-DEP...");
 
-    // Prepare a simple SELECT APDU for testing
-    uint8_t apdu[] = {0x00, 0xA4, 0x04, 0x00, 0x07,
-                      0xD2, 0x76, 0x00, 0x00, 0x85, 0x01, 0x01, 0x00};
+    // APDU: SELECT your HCE AID
+    uint8_t selectApdu[] = {0x00, 0xA4, 0x04, 0x00, 0x07, 0xD2, 0x76, 0x00, 0x00, 0x85, 0x01, 0x01, 0x00};
+    uint8_t response[255];
+    uint8_t responseLength = sizeof(response);
 
-    uint8_t response[64];
-    uint8_t responseLen;
+    success = nfc.inDataExchange(selectApdu, sizeof(selectApdu), response, &responseLength);
 
-    if (nfc.inDataExchange(apdu, sizeof(apdu), response, &responseLen)) {
-      Serial.print("APDU Response ("); Serial.print(responseLen); Serial.print(" bytes): ");
-      for (uint8_t i = 0; i < responseLen; i++) {
-        Serial.print(response[i], HEX); Serial.print(" ");
+    if (success && responseLength >= 2 && response[responseLength - 2] == 0x90 && response[responseLength - 1] == 0x00) {
+      Serial.println("ISO-DEP / Type 4 card confirmed!");
+      Serial.print("SELECT AID APDU response: ");
+      for (int i = 0; i < responseLength; i++) {
+        Serial.print(response[i], HEX);
+        Serial.print(" ");
       }
       Serial.println();
-    } else {
-      Serial.println("Failed to send APDU to HCE device.");
-    }
 
-    delay(2000); // avoid repeated polling
-  } else {
-    Serial.println("No HCE device found.");
+      // APDU: SELECT the NDEF file
+      uint8_t selectFileApdu[] = {0x00, 0xA4, 0x00, 0x0C, 0x02, 0xE1, 0x04};
+      responseLength = sizeof(response);
+      success = nfc.inDataExchange(selectFileApdu, sizeof(selectFileApdu), response, &responseLength);
+
+      if (!success || responseLength < 2 || response[responseLength - 2] != 0x90) {
+        Serial.println("Failed to select NDEF file.");
+        return;
+      }
+
+      Serial.println("NDEF file selected successfully.");
+
+      // Read NDEF length (first 2 bytes)
+      uint8_t readNlenApdu[] = {0x00, 0xB0, 0x00, 0x00, 0x02};
+      responseLength = sizeof(response);
+      success = nfc.inDataExchange(readNlenApdu, sizeof(readNlenApdu), response, &responseLength);
+
+      if (!success || responseLength < 4) {
+        Serial.println("Failed to read NDEF length");
+        return;
+      }
+
+      uint16_t ndefLength = (response[0] << 8) | response[1];
+      Serial.print("NDEF length: ");
+      Serial.println(ndefLength);
+
+      // Read NDEF in chunks (max 240 bytes per APDU)
+      uint16_t offset = 0;
+      while (offset < ndefLength) {
+        uint8_t chunkSize = (ndefLength - offset > 240) ? 240 : (ndefLength - offset);
+        uint8_t readApdu[] = {0x00, 0xB0, (uint8_t)(offset >> 8), (uint8_t)(offset & 0xFF), chunkSize};
+
+        responseLength = sizeof(response);
+        success = nfc.inDataExchange(readApdu, sizeof(readApdu), response, &responseLength);
+
+        if (!success || responseLength < 2) {
+          Serial.println("Failed reading NDEF chunk");
+          break;
+        }
+
+        // Print chunk as ASCII, skip last two SW1 SW2 bytes (0x90 0x00)
+        for (int i = 0; i < responseLength - 2; i++) {
+          Serial.print((char)response[i]);
+        }
+
+        offset += chunkSize;
+        delay(10); // avoid PN532 timeout
+      }
+
+      Serial.println("\n--- End of NDEF payload ---");
+    } else {
+      Serial.println("Not an ISO-DEP / Type 4 card, ignoring...");
+    }
   }
+
+  delay(500);
 }
