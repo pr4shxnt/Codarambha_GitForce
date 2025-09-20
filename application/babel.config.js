0ead2ce99ead89748d1030f23b4471091410ba92
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-09-21 00:02:52 +0545

    application: updates

--- application/babel.config.js
+++ application/babel.config.js
@@ -2,6 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
-    plugins: ['react-native-reanimated/plugin'],
   };
 };
