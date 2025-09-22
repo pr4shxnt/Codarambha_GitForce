b47f80688c17b620448a14de9481a3c1b439d214
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-09-22 20:47:14 +0545

    backend: cors issue solved

--- Backend/server.js
+++ Backend/server.js
@@ -10,23 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://transitpay.prashantadhikari7.com.np",
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
