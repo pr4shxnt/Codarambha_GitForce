b8e92efb0fea47423fe6b68100b1f8af74eb9c0f
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-09-25 21:56:44 +0545

    backend: wallet controller

---
+++ Backend/controllers/wallet.controller.js
@@ -0,0 +1,28 @@
+const Wallet = require("../models/wallet.model");
+const jwt = require("jsonwebtoken");
+
+exports.getWalletBalance = async (req, res) => {
+  try {
+    const { token } = req.body;
+
+    // uncomment this on prod
+    //  jwt.verify(token, process.env.TRANSITPAY_TOKEN_GENERATION_SECRET);
+
+    const decoded = jwt.decode(token);
+
+    const userId = decoded._id;
+    const userType = decoded._userType;
+
+    const wallet = await Wallet.find({ userType, userId });
+
+    if (!wallet) {
+      res
+        .status(404)
+        .json({ message: "Error. The wallet requested doesn't exist" });
+    }
+
+    return res.status(201).json({ walletBalance: wallet.balance });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error." });
+  }
+};
