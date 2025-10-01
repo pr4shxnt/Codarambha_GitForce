674eeb0513e3b81cfe612912fd0bc8ba0ad18c44
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-10-01 19:20:31 +0545

    damn

--- Backend/models/wallet.model.js
+++ Backend/models/wallet.model.js
@@ -1,43 +1,45 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const walletSchema = new mongoose.Schema(
-  {
-    balance: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-    userType: {
-      type: String,
-      enum: ["User", "Org", "Ward"],
-      required: true,
-    },
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      refPath: userType,
-      required: true,
-    },
-    history: [
-      {
-        title: {
-          type: String,
-        },
-        amount: {
-          type: Number,
-        },
-        uuid: {
-          type: String,
-        },
-        date: {
-          type: Date,
-        },
-        remarks: {
-          type: String,
-        },
-      },
-    ],
-  },
-  { timestamps: true }
-);
+// const walletSchema = new mongoose.Schema(
+//   {
+//     balance: {
+//       type: Number,
+//       required: true,
+//       default: 0,
+//     },
+//     userType: {
+//       type: String,
+//       enum: ["User", "Org", "Ward"],
+//       required: true,
+//     },
+//     userId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       refPath: userType,
+//       required: true,
+//     },
+//     history: [
+//       {
+//         title: {
+//           type: String,
+//         },
+//         amount: {
+//           type: Number,
+//         },
+//         uuid: {
+//           type: String,
+//         },
+//         date: {
+//           type: Date,
+//         },
+//         remarks: {
+//           type: String,
+//         },
+//       },
+//     ],
+//   },
+//   { timestamps: true }
+// );
 
-module.exports = mongoose.model("Wallet", walletSchema);
+// module.exports = mongoose.model("Wallet", walletSchema);
+
+// block chain integration remains
