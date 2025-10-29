9b8c7086b1d354f729649476e1e13745904f9f80
pr4shxnt <student@prashantadhikari7.com.np>
2025-10-29 15:07:07 +0545

    initialize

---
+++ application/components/Root/MainAppNavigation.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MainAppNavigation = () => {
+  return (
+    <View>
+      <Text>MainAppNavigation</Text>
+    </View>
+  )
+}
+
+export default MainAppNavigation
+
+const styles = StyleSheet.create({})
